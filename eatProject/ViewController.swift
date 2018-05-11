6a0ee50dfe20c387393874037f30535031e860b1
mrXAB <khrebtov@gmail.com>
2018-05-11 14:45:58 +0300

    initial local git

---
+++ eatProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eatProject
+//
+//  Created by Алексей on 11.05.2018.
+//  Copyright © 2018 Alexey Khrebtov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
