8c14f938988aa84a09a8cddf6a53c50e41ee111c
mrXAB <khrebtov@gmail.com>
2018-05-18 23:04:59 +0300

    Add alertControler

---
+++ eatProject/EatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EatTableViewCell.swift
+//  eatProject
+//
+//  Created by Алексей on 12.05.2018.
+//  Copyright © 2018 Alexey Khrebtov. All rights reserved.
+//
+
+import UIKit
+
+class EatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
